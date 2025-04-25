5842a5c0a6d71f3dce92da2403f48c19f159a740
liwlimuz <154435054+liwlimuz@users.noreply.github.com>
2025-04-25 16:15:26 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-module.exports={content:["./pages/**/*.{js,ts,jsx,tsx}"],theme:{extend:{}},plugins:[],};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
