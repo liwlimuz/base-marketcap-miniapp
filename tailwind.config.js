8551be63ae52f0bfb92b435638e0221c617d5275
liwlimuz <154435054+liwlimuz@users.noreply.github.com>
2025-04-26 12:44:44 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}'],
-  safelist: ['bg-gray-300','ring-2','ring-white','hover:-translate-y-2'],
+  safelist: ['bg-gray-200/30','backdrop-blur-sm','ring-1','ring-white/70','bg-gray-300','ring-2','ring-white','hover:-translate-y-2', ['bg-gray-300','ring-2','ring-white','hover:-translate-y-2'],
   theme: {
     extend: {
       colors: {
